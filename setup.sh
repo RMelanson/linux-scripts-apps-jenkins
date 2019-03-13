686415cc03f3fc4e57801f5ae75c16bc8abb1d72
Robin Melanson <remcogold@gmail.com>
2019-03-13 10:13:22 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,22 +5,10 @@
 . ./env/setEnv.sh
 
 #CHECK IF JENKINS INSTALLED AND RETURN IF INSTALLED
-. ./utils/exitIfInstalled.sh $wfHome
-
-# INSTALL REQUIRED DEVELOPMENT LIBRARIES
-./installs/installJava8.sh
-
-# SET UP JENKINS ADMIN USER
-./installs/addPkgUser.sh $wfOwner $wfGroup $wfHome $pkg
+. ./utils/exitIfInstalled.sh $jenkinsHome
 
 # DOWNLOAD AND INSTALL JENKINS 10
-./installs/installjBoss10.sh $wfOwner $wfGroup $wfHome $wfLog
-
-# SET UP JENKINS REMOTING CONFIGURATION 
-./installs/configjBossRemoting.sh $wfHome $wfAdmin
-
-# CONFIGURE JENKINS AS A SERVICE AND START JENKINS SERVICE
-./installs/addjBossAsService.sh $wfHome 
+./installs/installJenkins.sh
 
 # START JENKINS SERVICE
 service jenkins start
