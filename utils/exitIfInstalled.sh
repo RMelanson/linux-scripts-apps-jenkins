94daef94c77bfa61ddbb24d0a5789f30d9a5c8de
Robin Melanson <remcogold@gmail.com>
2019-03-13 23:11:09 -0400

    Update exitIfInstalled.sh

--- utils/exitIfInstalled.sh
+++ utils/exitIfInstalled.sh
@@ -2,7 +2,10 @@
 # VALIDATE THE INSTALLATION HOME DIRECTORY AND IF EXISTS, EXIT SCRIPT
 
 jenkinsHome=$1
-if [ -e $jenkinsLock ]; then
+if [ -e $jenkinsLock ]
+then
+    echo installing Jenkins
+else
     echo Jenkins Already installed EXITING
     return
 fi
