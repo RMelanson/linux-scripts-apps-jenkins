3514121e01e92c7895b7ba938e26bd3dd2f445ae
Robin Melanson <remcogold@gmail.com>
2019-03-13 22:45:19 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,13 +2,17 @@
 # INSTALL JENKINS ON LINUX WITH REMOTE ACCESS AS A SERVICE
 
 # SETUP WILDFLY CONFIGURATION ENVIRONMENT AND PARAMETERS
+echo Setup.sh:  EXECUTING . ./env/setEnv.sh
 . ./env/setEnv.sh
 
 #CHECK IF JENKINS INSTALLED AND RETURN IF INSTALLED
+echo Setup.sh:  EXECUTING . ./utils/exitIfInstalled.sh $jenkinsHome
 . ./utils/exitIfInstalled.sh $jenkinsHome
 
 # DOWNLOAD AND INSTALL JENKINS 10
-./installs/installJenkins.sh
+echo Setup.sh:  EXECUTING . ./installs/installJenkins.sh
+. ./installs/installJenkins.sh
 
 # START JENKINS SERVICE
+echo Setup.sh:  EXECUTING service jenkins start
 service jenkins start
