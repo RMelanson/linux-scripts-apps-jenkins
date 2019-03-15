bacdd8f8f1c18a3afd1b20700c25d1fef33a48b8
Robin Melanson <remcogold@gmail.com>
2019-03-14 21:25:12 -0400

    Update jenkinsBootstrap.sh

--- jenkinsBootstrap.sh
+++ jenkinsBootstrap.sh
@@ -16,8 +16,6 @@ yum install git -y
 # SETUP ENVIRONMENT AND PARAMETERS
 . ./env/setEnv.sh
 
-gitRepo="linux-scripts-apps-jenkins"
-installDir="/tmp/scripts/apps/JENKINS"
 if [ -f ~/.ssh/gitHub.key ]; then
    clone="git clone git@github.com:RMelanson/"
 else
