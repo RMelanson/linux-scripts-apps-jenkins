3d6426276777da7a75b405db42bc3990b284546e
Robin Melanson <remcogold@gmail.com>
2019-03-26 15:40:52 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -8,5 +8,5 @@ installDir="/tmp/scripts/apps/$pkg"
 jenkinsLock=/var/lock/subsys/jenkins
 JENKINS_WAR="/usr/lib/jenkins/jenkins.war"
 #jenkinsServer Options: STAND_ALONE, TOMCAT, JBOSS, WEBSPHERE, WEBLOGIC
-jenkinsServer=STAND_ALONE
+#jenkinsServer=STAND_ALONE
 jenkinsServer=JBOSS
