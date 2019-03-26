377c9049031d3920fb7fe551284ace1333c35717
Robin Melanson <remcogold@gmail.com>
2019-03-26 14:33:41 -0400

    Update setJenkinsServer.sh

--- installs/setJenkinsServer.sh
+++ installs/setJenkinsServer.sh
@@ -1,5 +1,27 @@
 server=$1
 
-# START JENKINS AS A SERVICE
-echo Setup.sh:  EXECUTING service jenkins start
-service jenkins start
+# SERVERS AVAILABLE STAND_ALONE, TOMCAT, JBOSS, WEBSPHERE, WEBLOGIC
+case "$server" in
+   STAND_ALONE)
+        echo STARTING JENKINS AS A SERVICE
+        echo Setup.sh:  EXECUTING service jenkins start
+        service jenkins start
+   ;;
+   TOMCAT)
+        echo $server not yet implemented 
+   ;;
+   sshPubKey)
+        echo $server not yet implemented 
+   ;;
+   JBOSS)
+        echo $server not yet implemented 
+   ;;
+   WEBLOGIC)
+        echo $server not yet implemented 
+   ;;
+   *) echo ERROR: Unknown Server = $server
+        exit 1
+   ;;
+esac
+
+
