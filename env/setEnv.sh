3791a2fd002bc5fe3c7d68029640f1f9206d4f17
Robin Melanson <remcogold@gmail.com>
2019-04-22 13:04:52 -0400

    Update setEnv.sh

--- env/setEnv.sh
+++ env/setEnv.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # JENKINS CONFIGURATION PARAMETERS
-pkg=JENKINS
 bootstrap="jenkinsBootstrap.sh"
 gitRepo="linux-scripts-apps-jenkins"
-installDir="/tmp/scripts/apps/$pkg"
+
+#SET UP INSTALLATION DIRECTORY`
+pkg=JENKINS
+scriptType="utils"
+parentDir="/tmp/scripts/$scriptType/"
+installDir="$parentDir/$pkg"
+
 jenkinsLock=/var/lock/subsys/jenkins
 JENKINS_WAR="/usr/lib/jenkins/jenkins.war"
 #jenkinsServer Options: STAND_ALONE, TOMCAT, JBOSS, WEBSPHERE, WEBLOGIC
