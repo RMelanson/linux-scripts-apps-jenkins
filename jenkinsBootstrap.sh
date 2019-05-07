f5ab5066c302923cd6e418a14f8a5c541f818860
Robin Melanson <remcogold@gmail.com>
2019-05-07 14:32:00 -0400

    Update jenkinsBootstrap.sh

--- jenkinsBootstrap.sh
+++ jenkinsBootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 bootstrapDir=$PWD
-bootstrap=$bootstrapDir/ZZZZZZZZZZZZZZZZ.sh
+bootstrap=$bootstrapDir/jenkinsBootstrap.sh
 
 # Ensure script is running under root
 if [ "$EUID" -ne 0 ]
